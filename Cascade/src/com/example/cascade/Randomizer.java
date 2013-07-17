822e3699a2cd4fa2abb1bf5f418c69d41fc7221c
Melchior <Melchior@192.168.0.103>
2013-07-16 18:07:33 -0700

    Randomizer updated: Beat the Clock game now works as intended.

--- Cascade/src/com/example/cascade/Randomizer.java
+++ Cascade/src/com/example/cascade/Randomizer.java
@@ -6,32 +6,48 @@ import java.util.Random;
 
 public class Randomizer{
 
-public void random (ToggleButton position1, ToggleButton position2, ToggleButton position3)
-{
+	int colorArray[] = {R.drawable.red_circle, R.drawable.green_circle, R.drawable.blue_circle};
+	
+	public void random (ToggleButton position1, ToggleButton position2, ToggleButton position3){
 
-int colorArray[] = {R.drawable.red_circle, R.drawable.green_circle, R.drawable.blue_circle};
+		int colorArray[] = {R.drawable.red_circle, R.drawable.green_circle, R.drawable.blue_circle};
+		int randomColor, randomColor2, randomColor3 = colorArray[0];
+		Random rand = new Random();
 
-Random rand = new Random();
+		do{
+			randomColor = rand.nextInt(colorArray.length);
+			position1.setButtonDrawable(colorArray[randomColor]);
+			
+			setAlpha(randomColor, position1);
 
-int randomColor, randomColor2, randomColor3 = colorArray[0];
+			do{
+				randomColor2 = rand.nextInt(colorArray.length);
+				position2.setButtonDrawable(colorArray[randomColor2]);
+				
+				setAlpha(randomColor2, position2);
+				
+			}while(randomColor2 == randomColor);
 
-do{
-randomColor = rand.nextInt(colorArray.length);
-position1.setButtonDrawable(colorArray[randomColor]);
+			do{
+				randomColor3 = rand.nextInt(colorArray.length);
+				position3.setButtonDrawable(colorArray[randomColor3]);
+				
+				setAlpha(randomColor3, position3);
+				
+			}while(randomColor3 == randomColor || randomColor3 == randomColor2);
 
-do{
-randomColor2 = rand.nextInt(colorArray.length);
-position2.setButtonDrawable(colorArray[randomColor2]);
-}while(randomColor2 == randomColor);
-
-do{
-randomColor3 = rand.nextInt(colorArray.length);
-position3.setButtonDrawable(colorArray[randomColor3]);
-}while(randomColor3 == randomColor || randomColor3 == randomColor2);
-
-}while(randomColor == 0 && randomColor2 == 1 && randomColor3 == 2);
-
+		}while(randomColor == 0 && randomColor2 == 1 && randomColor3 == 2);
+	}
+	
+	public void setAlpha(int rc, ToggleButton pos){
+		if (colorArray[rc] == R.drawable.red_circle){
+			pos.setAlpha(100);
+		}else if (colorArray[rc] == R.drawable.green_circle){
+			pos.setAlpha(99);
+		}else if (colorArray[rc] == R.drawable.blue_circle){
+			pos.setAlpha(98);}
+	}
 }
-
-}
+		
+		
 
